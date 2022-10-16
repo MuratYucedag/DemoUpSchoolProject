9cbdf247f6eb5f411162758f9f55cc23d0da007a
MuratYucedag <MurattYucedag@users.noreply.github.com>
2022-10-16 13:54:15 +0300

    Add project files.

---
+++ DemoUpSchoolProject/Controllers/DefaultController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DemoUpSchoolProject.Controllers
+{
+    public class DefaultController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Index2()
+        {
+            return View();
+        }
+
+        public ActionResult Index3()
+        {
+            return View();
+        }
+
+        public ActionResult Index4()
+        {
+            return View();
+        }
+    }
+}
---
+++ DemoUpSchoolProject/Controllers/ServicesController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DemoUpSchoolProject.Models.Entities;
+
+namespace DemoUpSchoolProject.Controllers
+{
+    public class ServicesController : Controller
+    {
+        /*
+         ToList
+         Add
+         Remove
+         Where         
+         */
+
+        UpSchoolDbPortfolioEntities db = new UpSchoolDbPortfolioEntities();
+        public ActionResult Index()
+        {
+            var values = db.TblServices.ToList();
+            return View(values);
+        }
+
+        [HttpGet]
+        public ActionResult AddService()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddService(TblServices p)
+        {
+            db.TblServices.Add(p);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult DeleteService(int id)
+        {
+            var values = db.TblServices.Find(id);
+            db.TblServices.Remove(values);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
